1c6d583cb0165a001544bfd3db5fe0f098a99711
mbeylin <mark_beylin@hotmail.com>
2017-05-23 19:51:00 -0400

    Created a factory for any generalized bounty

---
+++ contracts/BountyFactory.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.4.11;
+import "./Factory.sol";
+import "./StandardBounty.sol";
+
+
+/// @title Code bug bounties factory, concept by Stefan George - <stefan.george@consensys.net>
+/// @author Gonçalo Sá <goncalo.sa@consensys.net>
+contract BountyFactory is Factory {
+
+    /// @dev Allows multiple creations of bounties
+    /// @param _deadline the unix timestamp after which fulfillments will no longer be accepted
+    /// @param _contactInfo a string with contact info of the issuer, for them to be contacted if needed
+    /// @param _data the requirements of the bounty
+    /// @param _fulfillmentAmount the amount of wei to be paid out for each successful fulfillment
+    function create(
+        uint _deadline,
+        string _contactInfo,
+        string _data,
+        uint _fulfillmentAmount
+
+    )
+        public
+        returns (address bounty)
+    {
+        bounty = new StandardBounty(
+          _deadline,
+          _contactInfo,
+          _data,
+          _fulfillmentAmount
+        );
+        register(bounty);
+    }
+}
