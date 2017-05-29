688fadb096ccee8e07c815126df1ec7229bb5195
mbeylin <mark_beylin@hotmail.com>
2017-05-29 14:45:15 -0400

    fixed ordering during create calling

--- contracts/CodeBugBountyFactory.sol
+++ contracts/CodeBugBountyFactory.sol
@@ -12,14 +12,14 @@ contract CodeBugBountyFactory is Factory {
 
     /// @dev Allows multiple creations of code bug bounties
     /// @param _deadline the unix timestamp after which fulfillments will no longer be accepted
-    /// @param _data the requirements of the bounty
     /// @param _contactInfo the contact information of the issuer
+    /// @param _data the requirements of the bounty
     /// @param _fulfillmentAmount the amount of wei to be paid out for each successful fulfillment
     /// @param _bountiedContract the address of the contract to be bountied (with invariants check implemented)
     function create(
         uint _deadline,
-        string _data,
         string _contactInfo,
+        string _data,
         uint _fulfillmentAmount,
         Bountied _bountiedContract
     )
@@ -28,8 +28,8 @@ contract CodeBugBountyFactory is Factory {
     {
         bugBounty = new CodeBugBounty(
             _deadline,
-            _data,
             _contactInfo,
+            _data,
             _fulfillmentAmount,
             _bountiedContract
         );
