74fefc03e75c203c7fda843a330d5f745540b442
mbeylin <mark_beylin@hotmail.com>
2017-08-08 12:23:25 -0400

    bug fixes and cleaning up

--- contracts/TokenBounty.sol
+++ contracts/TokenBounty.sol
@@ -4,71 +4,73 @@ import "./StandardToken.sol";
 
 
 /// @title TokenBounty
+/// @dev Used to pay out individuals or groups in ERC20 tokens for task
+/// fulfillment through stepwise work submission, acceptance, and payment
 /// @dev extension of StandardBounty to pay out bounties with a given ERC20 token
 /// @author Mark Beylin <mark.beylin@consensys.net>
 contract TokenBounty is StandardBounty {
 
-	/*
-     * Storage
-     */
+  /*
+  * Storage
+  */
 
-    StandardToken public tokenContract;
+  StandardToken public tokenContract;
 
-    /*
-     * Modifiers
-     */
+  /*
+  * Modifiers
+  */
 
 
-    modifier amountEqualsValue(uint value) {
-        require(value  == tokenContract.allowance(msg.sender, this));
-        require(tokenContract.transferFrom(msg.sender, this, value));
+  modifier amountEqualsValue(uint value) {
+    require(value  == tokenContract.allowance(msg.sender, this));
+    require(tokenContract.transferFrom(msg.sender, this, value));
 
-        _;
+    _;
+  }
+
+  modifier validateFunding() {
+
+    uint total = 0;
+    for (uint i = 0 ; i < numMilestones; i++){
+      total += fulfillmentAmounts[i];
     }
 
-    modifier validateFunding() {
+    require (tokenContract.balanceOf(this) >= total);
 
-        uint total = 0;
-        for (uint i = 0 ; i < numMilestones; i++){
-            total += fulfillmentAmounts[i];
-        }
-
-        require (tokenContract.balanceOf(this) >= total);
-
-        _;
-    }
+    _;
+  }
 
 
-	/*
-     * Public functions
-     */
+  /*
+  * Public functions
+  */
 
-    /// @dev TokenBounty(): instantiates a new draft token bounty
-    /// @param _deadline the unix timestamp after which fulfillments will no longer be accepted
-    /// @param _contactInfo the contact information of the issuer
-    /// @param _data the requirements of the bounty
-    /// @param _fulfillmentAmount the amount of wei to be paid out for each successful fulfillment
-    /// @param _numMilestones the total number of milestones which can be paid out
-    /// @param _tokenAddress the address of the token contract
-    function TokenBounty(
-        uint _deadline,
-        string _contactInfo,
-        string _data,
-        uint[] _fulfillmentAmounts,
-        address _arbiter,
-        address _tokenAddress
+  /// @dev TokenBounty(): instantiates a new draft token bounty
+  /// @param _deadline the unix timestamp after which fulfillments will no longer be accepted
+  /// @param _contactInfo the contact information of the issuer
+  /// @param _data the requirements of the bounty
+  /// @param _fulfillmentAmount the amount of wei to be paid out for each successful fulfillment
+  /// @param _numMilestones the total number of milestones which can be paid out
+  /// @param _tokenAddress the address of the token contract
+  function TokenBounty(
+    uint _deadline,
+    string _contactInfo,
+    string _data,
+    uint[] _fulfillmentAmounts,
+    address _arbiter,
+    address _tokenAddress
     )
-    	StandardBounty(
-    		_deadline,
-    		_contactInfo,
-      	_data,
-      	_fulfillmentAmounts,
-        _numMilestones,
-        _arbiter
-    	)
-    {
-        tokenContract = StandardToken(_tokenAddress);
-    }
+    StandardBounty(
+      _deadline,
+      _contactInfo,
+      _data,
+      _fulfillmentAmounts,
+      _numMilestones,
+      _arbiter
+      )
+  {
+    tokenContract = StandardToken(_tokenAddress);
+  }
 
 
     /// @dev acceptFulfillment(): accept a given fulfillment, and send
@@ -76,32 +78,32 @@ contract TokenBounty is StandardBounty {
     /// @param _fulfillmentId the index of the fulfillment being accepted
     /// @param _milestoneId the id of the milestone being paid
     function fulfillmentPayment(uint _fulfillmentId, uint _milestoneId)
-        public
-        validateFulfillmentArrayIndex(_fulfillmentId, _milestoneId)
-        validateMilestoneIndex(_milestoneId)
-        onlyFulfiller(_fulfillmentId, _milestoneId)
-        checkFulfillmentIsApprovedAndUnpaid(_fulfillmentId, _milestoneId)
+    public
+    validateFulfillmentArrayIndex(_fulfillmentId, _milestoneId)
+    validateMilestoneIndex(_milestoneId)
+    onlyFulfiller(_fulfillmentId, _milestoneId)
+    checkFulfillmentIsApprovedAndUnpaid(_fulfillmentId, _milestoneId)
     {
-        tokenContract.transfer(fulfillments[_milestoneId][_fulfillmentId].fulfiller, fulfillmentAmounts[_milestoneId]);
-        fulfillments[_milestoneId][_fulfillmentId].paid = true;
+      tokenContract.transfer(fulfillments[_milestoneId][_fulfillmentId].fulfiller, fulfillmentAmounts[_milestoneId]);
+      fulfillments[_milestoneId][_fulfillmentId].paid = true;
 
-        numPaid[_milestoneId]++;
+      numPaid[_milestoneId]++;
 
-        FulfillmentPaid(msg.sender, fulNum);
+      FulfillmentPaid(msg.sender, fulNum);
     }
 
     /// @dev killBounty(): drains the contract of it's remaining
     /// funds, and moves the bounty into stage 3 (dead) since it was
     /// either killed in draft stage, or never accepted any fulfillments
     function killBounty()
-        public
-        onlyIssuer
+    public
+    onlyIssuer
     {
-        tokenContract.transfer(tokenContract.balanceOf(this) - unpaidAmount());
+      tokenContract.transfer(tokenContract.balanceOf(this) - unpaidAmount());
 
-        transitionToState(BountyStages.Dead);
+      transitionToState(BountyStages.Dead);
 
-        BountyKilled();
+      BountyKilled();
     }
 
-}
+  }
