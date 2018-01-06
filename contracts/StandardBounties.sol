7547cd6228089ba3df24eb8b7c9544fa156069af
mbeylin <mark_beylin@hotmail.com>
2018-01-05 21:00:04 -0500

    added contribution refundability (if users so desire)

--- contracts/StandardBounties.sol
+++ contracts/StandardBounties.sol
@@ -10,13 +10,19 @@ contract StandardBounty {
    * Storage
    */
 
-  address masterCopy;
+  address public masterCopy;
 
-  address issuer;
-  string data;
-  address arbiter;
+  address public issuer;
+  string public data;
+  address public arbiter;
 
-  Fulfillment[] fulfillments;
+  bool public hasPaidOut;
+
+  Fulfillment[] public fulfillments;
+
+  Contribution[] public contributions;
+
+
 
   /*
    * Events
@@ -33,6 +39,14 @@ contract StandardBounty {
   struct Fulfillment {
       address fulfiller;
       string data;
+  }
+
+  struct Contribution {
+      address contributor;
+      uint[] amounts;
+      StandardToken[] tokens;
+      bool refunded;
+      mapping (address => bool) hasContributed;
   }
 
   /*
@@ -69,6 +83,21 @@ contract StandardBounty {
       _;
   }
 
+  modifier hasNotPaid(){
+      require(!hasPaidOut);
+      _;
+  }
+
+  modifier onlyContributor(uint _contributionId){
+      require(contributions[_contributionId].contributor == msg.sender);
+      _;
+  }
+
+  modifier notYetRefunded(uint _contributionId){
+      require(!contributions[_contributionId].refunded);
+      _;
+  }
+
   /*
    * Public functions
    */
@@ -90,9 +119,48 @@ contract StandardBounty {
   {
     require(issuer == address(0));
     require(_issuer != address(0));
+    // an issuer of a bounty is only 0x0 when it is uninitialized,
+    // so this check prevents initialization from being called multiple times
+
     issuer = _issuer;
     data = _data;
     arbiter = _arbiter;
+  }
+
+  function refundableContribute(uint[] _amounts, StandardToken[] _tokens)
+  public
+  payable
+  {
+      require(_amounts.length == _tokens.length);
+
+      contributions.push(Contribution(msg.sender, _amounts, _tokens, false));
+      uint contributionId = contributions.length - 1;
+      for (uint i = 0; i < _amounts.length; i++){
+          require(!contributions[contributionId].hasContributed[_tokens[i]]);
+          contributions[contributionId].hasContributed[_tokens[i]] = true;
+          if (_tokens[i] == address(0)){
+              require(msg.value == _amounts[i]);
+          } else {
+              require(_tokens[i].transferFrom(msg.sender, this, _amounts[i]));
+          }
+      }
+  }
+
+  function refundContribution(uint _contributionId)
+  public
+  payable
+  hasNotPaid
+  onlyContributor(_contributionId)
+  notYetRefunded(_contributionId)
+  {
+      contributions[_contributionId].refunded = true;
+      for (uint i = 0; i < contributions[_contributionId].amounts.length; i++){
+          if (contributions[_contributionId].tokens[i] == address(0)){
+              contributions[_contributionId].contributor.transfer(contributions[_contributionId].amounts[i]);
+          } else {
+              require(contributions[_contributionId].tokens[i].transfer(contributions[_contributionId].contributor, contributions[_contributionId].amounts[i]));
+          }
+      }
   }
 
   function fulfillBounty(address _fulfiller, string _data)
@@ -135,6 +203,7 @@ contract StandardBounty {
       validateFulfillmentArrayIndex(_fulfillmentId)
       onlyIssuerOrArbiter
   {
+      hasPaidOut = true;
       for (uint256 i = 0; i < _payoutTokens.length; i++){
         uint toPay;
         if (_payoutTokens[i] == address(0x0)){
@@ -178,6 +247,31 @@ contract StandardBounty {
       data = _data;
       BountyChanged();
   }
+
+  function changeIssuer(address _issuer)
+      public
+      onlyIssuer
+  {
+      issuer = _issuer;
+      BountyChanged();
+  }
+
+  function changeArbiter(address _arbiter)
+      public
+      onlyIssuer
+  {
+      arbiter = _arbiter;
+      BountyChanged();
+  }
+
+  function changeData(string _data)
+      public
+      onlyIssuer
+  {
+      data = _data;
+      BountyChanged();
+  }
+
   function changeMasterCopy(StandardBounty _masterCopy)
         public
         onlyIssuer
@@ -189,11 +283,12 @@ contract StandardBounty {
   function getBounty()
         public
         constant
-        returns (address, address, string, Fulfillment[])
+        returns (address, address, string, Fulfillment[], bool)
     {
         return (issuer,
                 arbiter,
                 data,
-                fulfillments);
+                fulfillments,
+                hasPaidOut);
     }
 }
