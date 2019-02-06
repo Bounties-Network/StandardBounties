53ce9a789dc6c582dbecf2260ab8234bf2c84899
mbeylin <mark_beylin@hotmail.com>
2019-02-05 20:55:21 -0500

    WIP meta txns

--- contracts/StandardBountiesFactory.sol
+++ contracts/StandardBountiesFactory.sol
@@ -34,5 +34,50 @@ contract StandardBountiesFactory {
   {
     return bounties.length;
   }
+  function metaFulfill(bytes signature, address[] _fulfillers, string _data, uint256 nonce) public returns (bool) {
+    bytes32 metaHash = keccak256(abi.encodePacked(address(this),"metaFulfill", _fulfillers, _data, _nonce));
+    address signer = getSigner(metaHash,signature);
+    //make sure signer doesn't come back as 0x0
+    require(signer!=address(0));
+    require(nonce == replayNonce[signer]);
+    replayNonce[signer]++;
+    internalFulfill(signer, to, value);
 
+  }
+
+  function internalFulfill(address _submitter, address[] _fulfillers, string _data)
+      public
+      validateNotTooManyFulfillments
+  {
+      require(now < deadline);
+
+      fulfillments.push(Fulfillment(_fulfillers, false));
+
+      BountyFulfilled((fulfillments.length - 1), msg.sender, _data);
+  }
+
+
+  function getSigner(bytes32 _hash, bytes _signature) internal pure returns (address){
+    bytes32 r;
+    bytes32 s;
+    uint8 v;
+    if (_signature.length != 65) {
+      return address(0);
+    }
+    assembly {
+      r := mload(add(_signature, 32))
+      s := mload(add(_signature, 64))
+      v := byte(0, mload(add(_signature, 96)))
+    }
+    if (v < 27) {
+      v += 27;
+    }
+    if (v != 27 && v != 28) {
+      return address(0);
+    } else {
+      return ecrecover(keccak256(
+        abi.encodePacked("\x19Ethereum Signed Message:\n32", _hash)
+      ), v, r, s);
+    }
+  }
 }
